16fa12daad4967aea5340677993a70d7a2befdb5
Daniel Swiatek <129677738+swtn@users.noreply.github.com>
2025-04-23 14:54:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 class Task {
-    constructor(id, content, user, status = "pending"){
+    constructor(id, content, user, status = "pending", priority = "low"){
         this.id = id;
         this.content = content;
         this.user = user;
         this.status = status;
+        this.priority = priority;
         this.createdAt = new Date();
     }
 }
@@ -13,9 +14,9 @@ class TaskManager {
         this.tasks = [];
     }
 
-    addTask(content, user) {
+    addTask(content, user, priority) {
         const id = Date.now();
-        const task = new Task(id, content, user);
+        const task = new Task(id, content, user, priority);
         this.tasks.push(task);
         this.saveToLocalStorage();
         return task;
@@ -49,28 +50,32 @@ class TaskManager {
     saveToLocalStorage() {
         localStorage.setItem('tasks', JSON.stringify(this.tasks));
     }
-
-    loadFromLocalStorage(){
-        const data = localStorage.getItem('tasks');
-        if(data) {
-            const parsed = JSON.parse(data);
-            this.tasks = parsed.map(obj => {
-                const task = new Task(obj.id, obj.content, obj.user, obj.status);
-                task.createdAt = new Date(obj.createdAt);
-                return task;
-            });
-        }
+          
+    loadFromLocalStorage() {
+    const data = localStorage.getItem('tasks');
+    if(data){
+      const parsed = JSON.parse(data);
+      this.tasks = parsed.map(obj => {
+        const task = new Task(obj.id, obj.content, obj.user, obj.status);
+        task.createdAt = new Date(obj.createdAt);
+        return task;
+      });
     }
+  }
 }
 const taskManager = new TaskManager();
-
+const taskList = document.getElementById('task-list');
 const form = document.getElementById('task-form');
 const contentInput = document.getElementById('task-content');
 const userInput = document.getElementById('task-user');
-const taskList = document.getElementById('task-list');
 const filterSelect = document.getElementById('filter-status');
 const filterUser = document.getElementById('filter-user');
+const priorityInput = document.getElementById('task-priority');
+const filterPriority = document.getElementById('filter-priority');
 
+taskManager.loadFromLocalStorage();
+renderTasks();
+localStorage.getItem('tasks');
 filterSelect.addEventListener('change', () => {
     renderTasks();
 })
@@ -79,13 +84,17 @@ filterUser.addEventListener('change', () => {
     renderTasks();
 })
 
+filterPriority.addEventListener('change', () => {
+    renderTasks();
+})
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const content = contentInput.value;
     const user = userInput.value;
-
-    const task = taskManager.addTask(content, user);
+    const priority = priorityInput.value;
+    const task = taskManager.addTask(content, user, priority);
     renderTasks();
 
     contentInput.value = '';
@@ -96,6 +105,7 @@ function renderTasks(){
 
     const selectedStatus = filterSelect.value;
     const selectedUser = filterUser.value;
+    const selectedPriority = filterPriority.value;
 
     let tasksToShow = taskManager.getAllTasks();
 
@@ -107,10 +117,15 @@ function renderTasks(){
         tasksToShow = tasksToShow.filter(task => task.user === selectedUser);
     }
 
+    if(selectedPriority !== 'all') {
+    tasksToShow = tasksToShow.filter(task => task.priority === selectedPriority);
+    }
+
     tasksToShow.forEach(task => {
         const div = document.createElement('div');
         div.innerHTML = `
-        <p><strong>${task.content}</strong> [${task.status}] - ${task.user}</p>
+        <p><strong>${task.content}</strong> [${task.status}] - ${task.user}
+        <span class="priority ${task.priority}">Priorytet: ${task.priority}</span></p>
         <button onclick="toogleTask(${task.id})">Zmień status</button>
         <button onclick="deleteTask(${task.id})">Usuń</button>
         <hr />
